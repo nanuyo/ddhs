d885ec78887b89bf7e8c9d0d19291f0dfac5cf81
hjpark <hjparkg71@gmail.com>
2024-05-15 20:48:13 +0900

    Update softap_webserver.c

--- v001/softap_webserver/softap_webserver.c
+++ v001/softap_webserver/softap_webserver.c
@@ -115,57 +115,57 @@ int main()
     int opt = 1;
     socklen_t addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
-    while (1)
+    start_softap_mode(ap_ssid, ap_password, ap_ip_address);
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
-        start_softap_mode(ap_ssid, ap_password, ap_ip_address);
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-        // Create socket
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-        {
-            perror("socket failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Set socket options
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-        {
-            perror("setsockopt SO_REUSEADDR");
-            close(server_fd);
-            exit(EXIT_FAILURE);
-        }
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        perror("setsockopt SO_REUSEADDR");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
 #ifdef SO_REUSEPORT
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) == -1)
-        {
-            perror("setsockopt SO_REUSEPORT");
-            close(server_fd);
-            exit(EXIT_FAILURE);
-        }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) == -1)
+    {
+        perror("setsockopt SO_REUSEPORT");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 #endif
 
-        // Configure address and port
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(PORT);
+    // Configure address and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-        // Bind the socket to the specified address
-        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-        {
-            perror("bind failed");
-            close(server_fd);
-            exit(EXIT_FAILURE);
-        }
+    // Bind the socket to the specified address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-        // Set the socket to listen for incoming connections
-        if (listen(server_fd, 3) < 0)
-        {
-            perror("listen");
-            close(server_fd);
-            exit(EXIT_FAILURE);
-        }
+    // Set the socket to listen for incoming connections
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-        printf("Server is listening on port %d\n", PORT);
+    printf("Server is listening on port %d\n", PORT);
 
+    while (1)
+    {
         // Accept incoming client connections
         new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
         if (new_socket < 0)
@@ -211,9 +211,9 @@ int main()
         }
 
         close(new_socket);
-
-        close(server_fd);
     }
+    close(server_fd);
+
     return 0;
 }
 
